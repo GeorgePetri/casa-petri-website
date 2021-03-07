6a10c38d61c7f353328afe9896999dfc194f49a2
George <t8_8t@icloud.com>
2021-03-07 16:50:31 +0000

    add contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>Contact</title>
+</svelte:head>
+
+<p>
+    <b>Telefon:</b> 0745006064
+</p>
+<p>
+    <b>>Adresa:</b> Constructorilor nr 12, Sanpetru, Brasov
+</p>
+<p>
+    <b>Email:</b> casapetri@gmail.com
+</p>
