f7ca2056b65ddf1813e0aaac709834c57001b055
George Petri <t8_8t@icloud.com>
2021-09-10 23:31:45 +0300

    add contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+    <title>Contact</title>
+</svelte:head>
+
+<h1>Contact</h1>
+<p>
+    <b>Telefon:</b> 0745006064
+</p>
+<p>
+    <b>Adresa:</b> Constructorilor nr 12, Sanpetru, Brasov
+</p>
+<p>
+    <b>Email:</b> casapetri@gmail.com
+</p>
