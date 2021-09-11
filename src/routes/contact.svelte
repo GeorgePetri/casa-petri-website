b44a3377b2ef36359c863aa3d11599189c132aa1
George Petri <t8_8t@icloud.com>
2021-09-11 20:27:37 +0300

    icons to contact page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -2,17 +2,22 @@
 	export const prerender = true;
 </script>
 
+<script lang="ts">
+	import Fa from 'svelte-fa';
+	import { faPhone, faMapMarkerAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+</script>
+
 <svelte:head>
     <title>Contact</title>
 </svelte:head>
 
 <h1>Contact</h1>
 <p>
-    <b>Telefon:</b> 0745006064
+    <b><Fa icon={faPhone} /> Telefon:</b> 0745006064
 </p>
 <p>
-    <b>Adresa:</b> Constructorilor nr 12, Sanpetru, Brasov
+    <b><Fa icon={faMapMarkerAlt} /> Adresa:</b> Constructorilor nr 12, Sanpetru, Brasov
 </p>
 <p>
-    <b>Email:</b> casapetri@gmail.com
+    <b><Fa icon={faEnvelope} /> Email:</b> casapetri@gmail.com
 </p>
