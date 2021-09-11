498c3a15f03a78d0ce9e43a94e5f1bba183176d8
George Petri <t8_8t@icloud.com>
2021-09-11 21:06:34 +0300

    add gallery

---
+++ src/lib/Gallery.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let images: string[];
+</script>
+
+<ul>
+	{#each images as image}
+		<li><img src={image} alt={image} /></li>
+	{/each}
+</ul>
--- src/routes/photos.svelte
+++ src/routes/photos.svelte
@@ -1,6 +1,12 @@
-
 <script context="module">
 	export const prerender = true;
 </script>
 
+<script lang="ts">
+	import Gallery from '$lib/Gallery.svelte';
+
+	const images: string[] = ['svelte-welcome.png', 'svelte-welcome.png'];
+</script>
+
 <h1>Poze</h1>
+<Gallery {images} />
