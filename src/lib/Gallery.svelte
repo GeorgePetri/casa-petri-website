8848d1bda0717552adacdf109ae20a4954ab0047
George Petri <t8_8t@icloud.com>
2021-11-30 14:43:30 +0200

    add gallery modal

--- src/lib/Gallery.svelte
+++ src/lib/Gallery.svelte
@@ -1,26 +1,31 @@
 <script lang="ts">
+	import GalleryModal from './GalleryModal.svelte';
+
 	export let images: string[];
-    export let imageWidth: number;
+	export let imageWidth: number;
+
+	let showingImage: string;
 </script>
 
+<GalleryModal bind:image={showingImage} />
 <div style="--image-width: {imageWidth}%">
 	{#each images as image}
-		<img src={image} alt={image} />
+		<img src={image} alt={image} on:click={() => (showingImage = image)} />
 	{/each}
 </div>
 
 <style>
 	div {
 		display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: center;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
 	}
 
-    img {
-        display: block;
-        width: var(--image-width);
-        height: 100%;
-        padding: 10px;
-    }
+	img {
+		display: block;
+		width: var(--image-width);
+		height: 100%;
+		padding: 10px;
+	}
 </style>
---
+++ src/lib/GalleryModal.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let image: string;
+</script>
+
+{#if image}
+	<div class="container">
+		<img src={image} alt={image} on:click={() => (image = null)} />
+	</div>
+{/if}
+
+<style>
+	div.container {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	img {
+		display: block;
+		width: 80%;
+		height: 80%;
+	}
+</style>
